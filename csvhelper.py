0de0915f7d868856e4a8da8ea577ba917cd93bc3
owlen <oren@orengampel.com>
2014-06-03 21:14:07 +0300

    add used address to output and allow running on output while ignoring processed rows.

--- csvhelper.py
+++ csvhelper.py
@@ -6,7 +6,7 @@ from router import Router
 def getlatlng(address):
     latlngs = Router.getgeocode(address)
     if len(latlngs) > 0:
-        return (latlngs[0][u'lat'], latlngs[0][u'lon'])
+        return (address, (latlngs[0][u'lat'], latlngs[0][u'lon']))
     words = address.split(' ')[1:]
     if len(words) < 3:
         raise Exception('invalid address')
@@ -14,20 +14,25 @@ def getlatlng(address):
 
 def gettraveltime(address):
     try:
-        dst = getlatlng(address)
+        addr, dst = getlatlng(address)
+#         print("got addr:%s   ll: %s" % (addr, dst))
     except Exception as e:
         if 'invalid address' == str(e):
-            return
+            return ("",0)
         else:
             raise
-    return Router(src, dst, 'bicycle').getTravelTimeMin()
+    #     print("calculating for %s from %s to %s" % (addr, src, dst))
+    return (addr, Router(src, dst, 'bicycle').getTravelTimeMin())
 
 if __name__ == '__main__':
     from sys import argv, stdout
-    src = getlatlng(' '.join(argv[2:]))
-    print(argv[1])
+    _, src = getlatlng(' '.join(argv[2:]))
     with open(argv[1]) as f:
         from csv import writer, reader
         out = writer(stdout)
         for row in reader(f):
-            out.writerow(row + [gettraveltime(row[4])])
+            if row[15] == '':
+                r = gettraveltime(row[4])
+                row[15], row[16] = r
+
+            out.writerow(row)
