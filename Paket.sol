5fecf7dee18b3bee25ffc5c0a0866a6134c4cb35
owlen <oren@orengampel.com>
2018-01-16 11:01:15 +0200

    minor name changes

--- Paket.sol
+++ Paket.sol
@@ -21,6 +21,8 @@ contract Bul is MintableToken {
         uint256 deadline;
         uint256 offeredPayment;
         uint256 requiredCollateral;
+
+        // Balances in case of success and failure, indexed by payment and collateral.
         mapping (address => uint256) successBalances;
         address[] paymentBenificieries;
         address[] collateralRefundees;
@@ -31,28 +33,34 @@ contract Bul is MintableToken {
 
     Paket[] private pakets;
 
+    // Create a new "empty" paket.
     function create(
         address _recipient, uint256 _deadline, uint256 _offeredPayment, uint256 _requiredCollateral
     ) public returns (uint256) {
         require(_deadline > now);
+        // push returns the new length of the array, so we subtract 1 to get the new index.
+        // Also note the new emptyt arrays created for the new paket struct.
         return pakets.push(Paket(
             _recipient, _deadline, _offeredPayment, _requiredCollateral,
             new address[](0), new address[](0), new address[](0), new address[](0)
         )) - 1;
     }
 
+    // Modifier to functions that commit BULs.
     modifier commitBuls(uint256 _amount) {
         require(balanceOf(msg.sender) >= _amount);
         balances[msg.sender] -= _amount;
         _;
     }
 
+    // Modifier to functions that need to add balances from outside.
     modifier managePaketBalances(uint256 _paketIdx, address _successAddress, address _failAddress, uint256 _amount) {
         pakets[_paketIdx].successBalances[_successAddress] += _amount;
         pakets[_paketIdx].failBalances[_failAddress] += _amount;
         _;
     }
 
+    // Add payment to a paket.
     function commitPayment (uint256 _paketIdx, address _payee, uint256 _amount) public
         commitBuls(_amount) managePaketBalances(_paketIdx, _payee, msg.sender, _amount)
     {
@@ -60,6 +68,7 @@ contract Bul is MintableToken {
         pakets[_paketIdx].paymentRefundees.push(msg.sender);
     }
 
+    // Add collateral to a paket.
     function commitCollateral(uint256 _paketIdx, address _payee, uint256 _amount) public
         commitBuls(_amount) managePaketBalances(_paketIdx, msg.sender, _payee, _amount)
     {
@@ -67,6 +76,7 @@ contract Bul is MintableToken {
         pakets[_paketIdx].collateralRefundees.push(msg.sender);
     }
 
+    // Promise some of the payment previously promised to you to another.
     function relayPayment(uint256 _paketIdx, address _payee, uint256 _amount) public {
         require(pakets[_paketIdx].successBalances[msg.sender] >= _amount);
         pakets[_paketIdx].successBalances[msg.sender] -= _amount;
@@ -74,6 +84,9 @@ contract Bul is MintableToken {
         pakets[_paketIdx].paymentBenificieries.push(msg.sender);
     }
 
+    // Cover someone else's collateral (so he gets his BULs back).
+    // _amount must be equal or less than what he committed.
+    // For increasing collateral use commitCollateral.
     function coverCollateral(uint256 _paketIdx, address _payee, uint256 _amount) public commitBuls(_amount) {
         require(pakets[_paketIdx].successBalances[_payee] >= _amount);
         pakets[_paketIdx].successBalances[_payee] -= _amount;
@@ -82,10 +95,12 @@ contract Bul is MintableToken {
         balances[_payee] += _amount;
     }
 
+    // Check my balance in case of success and failure;
     function paketSelfInterest(uint256 _paketIdx) public view returns (uint256, uint256) {
         return (pakets[_paketIdx].successBalances[msg.sender], pakets[_paketIdx].failBalances[msg.sender]);
     }
 
+// Why can't we use this helper function?
 //    function _settle(address[] _benificiaries, mapping (address => uint256) _payments) private {
 //        for (uint256 idx = 0; idx < _benificiaries.length; idx++) {
 //            balances[_benificiaries[idx]] += _payments[_benificiaries[idx]];
@@ -93,6 +108,10 @@ contract Bul is MintableToken {
 //        }
 //    }
 
+<<<<<<< HEAD
+=======
+    // Refund all payments and forward all collaterals if the deadline has passed.
+>>>>>>> 88677980f52e9089a75dc322dfcb9cd2d5988b8a
     function refund(uint256 _paketIdx) public {
         require(pakets[_paketIdx].deadline < now);
         uint256 idx;
@@ -110,6 +129,10 @@ contract Bul is MintableToken {
         delete pakets[_paketIdx];
     }
 
+<<<<<<< HEAD
+=======
+    // Forward all payments and refund all collaterals if recipient agrees.
+>>>>>>> 88677980f52e9089a75dc322dfcb9cd2d5988b8a
     function pay(uint256 _paketIdx) public {
         require(pakets[_paketIdx].recipient == msg.sender);
         uint256 idx;
