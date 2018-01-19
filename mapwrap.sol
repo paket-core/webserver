87aef3e057e7213836b20948d20cb2e94b5c38b5
Israel <israel@paket.global>
2018-01-19 17:40:50 +0200

    Spaces.

--- mapwrap.sol
+++ mapwrap.sol
@@ -3,12 +3,10 @@ pragma solidity ^0.4.13;
 // Pass mappings to functions.
 // ---------------------------------------------------------------------
 
-
 contract Bul {
     struct MapWrap {
         mapping (string => uint256) stam;
     }
-    
     MapWrap m1;
     MapWrap m2;
 
@@ -16,11 +14,9 @@ contract Bul {
         m1.stam["one"] = 2;
         m2.stam["one"] = 4;
     }
-    
     function _t(mapping (string => uint256) _m) internal returns (uint256) {
         return _m.stam["one"];
     }
-    
     function f1() public view returns (uint256) {
         return _t(m1);
     }
