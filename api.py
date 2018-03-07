cb9e052b06afb25bec44657428259d7d6cd096e8
owlen <oren@orengampel.com>
2018-03-07 21:24:53 +0200

    solve catchall return 403 - the base url was wrong.

--- api.py
+++ api.py
@@ -26,6 +26,20 @@ APP.config['SWAGGER'] = {
 
 template = {
   "swagger": "3.0",
+
+  "info": {
+    "title": "PaKeT API",
+    "description": "This is a cool thing.",
+    "contact": {
+      "responsibleOrganization": "ME",
+      "responsibleDeveloper": "Me",
+      "email": "me@me.com",
+      "url": "www.me.com",
+    },
+    "termsOfService": "http://me.com/terms",
+    "version": VERSION
+  },
+
   "securityDefinitions": {
     "an_api_key thingy": {
       "type": "apiKey",
@@ -42,24 +56,10 @@ template = {
       }
     },
   },
-
-  "info": {
-    "title": "PaKeT API",
-    "description": "This is a cool thing.",
-    "contact": {
-      "responsibleOrganization": "ME",
-      "responsibleDeveloper": "Me",
-      "email": "me@me.com",
-      "url": "www.me.com",
-    },
-    "termsOfService": "http://me.com/terms",
-    "version": VERSION
-  },
   #"host": "mysite.com",  # overrides localhost:500
-  "basePath": "/api",  # base bash for blueprint registration
+  #"basePath": "/api",  # base bash for blueprint registration
   "schemes": [
     "http",
-    "https"
   ],
 }
 
@@ -240,11 +240,11 @@ def ratelimit_handler(error):
 @LIMITER.limit(limit_value="2000 per second")
 def catch_all_endpoint(path='index.html'):
     'All undefined endpoints try to serve from the static directories.'
+    LOGGER.warning('in catchall')
     for directory in STATIC_DIRS:
         if os.path.isfile(os.path.join(directory, path)):
             return flask.send_from_directory(directory, path)
     error = "Forbidden: /{}".format(path)
     if path[0] == 'v' and path[2] == '/' and path[1].isdigit and path[1] != VERSION:
-        error = "/{} - you are trying to access an unsupported version of the API ({}), please use /v{}/".format(
-            path, VERSION, path[1])
+        error = "/{} - you are trying to access an unsupported version of the API ({}), please use /v{}/".format(path, VERSION, VERSION)
     return flask.jsonify({'status': 403, 'error': error}), 403
