c79894b4a1111af2b0b7d01539ef2312590bab82
Israel <israel@paket.global>
2018-03-07 16:28:23 +0200

    Validators and env and more endpoints.

--- logger.py
+++ logger.py
@@ -4,16 +4,14 @@ import os
 
 import coloredlogs
 
-LOG_DIR_NAME = '/var/log'
-LOG_FILE_NAME = 'pkt.log'
+LOG_DIR_NAME = './'
+LOG_FILE_NAME = 'paket.log'
 FORMAT = '%(asctime)s %(levelname).3s: %(message)s - %(name)s +%(lineno)03d'
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
-LEVEL_NAME = 'DEBUG'
+LEVEL = logging.DEBUG
 
 def setup():
     'Setup the root logger.'
-    logger = logging.getLogger()
-    logger.handlers = []
     # Colored logs for terminal. Do this first, because it messes with the logger's level.
     stream_formatter = coloredlogs.ColoredFormatter(
         fmt=FORMAT, datefmt=DATE_FORMAT, level_styles={
@@ -22,10 +20,18 @@ def setup():
         }, field_styles={'name': {'color': 'cyan'}, 'lineno': {'color': 'cyan'}})
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(stream_formatter)
-    logger.addHandler(stream_handler)
 
+    # Logging to file.
     file_formatter = logging.Formatter(FORMAT, DATE_FORMAT)
     file_handler = logging.FileHandler(os.path.join(LOG_DIR_NAME, LOG_FILE_NAME))
     file_handler.setFormatter(file_formatter)
+
+    # Remove werkzeug and flask default handlers.
+    logging.getLogger('werkzeug').handlers = []
+    logging.getLogger('flask').handlers = []
+
+    logger = logging.getLogger()
+    logger.handlers = []
+    logger.addHandler(stream_handler)
     logger.addHandler(file_handler)
-    logger.setLevel(LEVEL_NAME)
+    logger.setLevel(LEVEL)
