b69a999570cf311ae92742f289adefe195184c5b
Israel <israel@paket.global>
2018-04-01 13:47:48 +0300

    Basic launch.

--- stellar/init.py
+++ stellar/init.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+# pylint: disable=missing-docstring,import-error
 import requests
 import stellar_base.address
 import stellar_base.asset
 import stellar_base.builder
 import stellar_base.keypair
+
+
+PERSIST = True
 
 
 def random_seed():
@@ -15,48 +19,109 @@ def get_keypair(seed):
     return stellar_base.keypair.Keypair.from_seed(seed)
 
 
+if PERSIST:
+    [ISSUER, LAUNCHER, COURIER, RECIPIENT] = [get_keypair(seed) for seed in [
+        'SC2PO5YMP7VISFX75OH2DWETTEZ4HVZOECMDXOZIP3NBU3OFISSQXAEP',
+        'SDJGBJZMQ7Z4W3KMSMO2HYEV56DJPOZ7XRR7LJ5X2KW6VKBSLELR7MRQ',
+        'SBOLPN4HNTCLA3BMRS6QG62PXZUFOZ5RRMT6LPJHUPGQLBP5PZY4YFIT',
+        'SA5OXLJ2JCX4PF3G5WKSG66CXJQXQFCT62NQJ747XET5E2FR6TVIE4ET']]
+else:
+    ISSUER = get_keypair(random_seed())
+    LAUNCHER = get_keypair(random_seed())
+    COURIER = get_keypair(random_seed())
+    RECIPIENT = get_keypair(random_seed())
+PARTICIPANTS = [LAUNCHER, COURIER, RECIPIENT]
+
+
 def get_details(pubkey):
     address = stellar_base.address.Address(pubkey)
     address.get()
     return address
 
 
-def trust(account, asset_code, issuer):
+def trust(account):
     builder = stellar_base.builder.Builder(secret=account.seed())
-    builder.append_trust_op(issuer, asset_code, None, account.address().decode())
+    builder.append_trust_op(ISSUER.address().decode(), 'BUL')
     builder.sign()
-    return builder.submit()
-    return get_details(account.address().decode()).balances
+    builder.submit()
 
 
-def fund_and_trust(account, asset_code, issuer):
+def get_bul_balance(account):
+    'Get acount BUL balance. Create, fund and trust if needed.'
     pubkey = account.address().decode()
     try:
         balances = get_details(pubkey).balances
-        if pubkey == issuer:
-            return balances
         for balance in balances:
-            if balance.get('asset_code') == 'BUL' and balance.get('asset_issuer') == issuer:
-                return balance
+            if balance.get('asset_code') == 'BUL' and balance.get('asset_issuer') == ISSUER.address().decode():
+                return float(balance['balance'])
+
+    # Create and fund non existing accounts.
     except stellar_base.utils.AccountNotExistError:
         request = requests.get("https://friendbot.stellar.org/?addr={}".format(pubkey))
         if request.status_code != 200:
             raise Exception("Funding request failed - {}".format(request.content))
-    return trust(account, asset_code, issuer)
+
+    # If we are here, the account is not trusted. Fix it if possible and call again.
+    if pubkey == ISSUER.address().decode():
+        return None
+    else:
+        trust(account)
+    return get_bul_balance(account)
 
 
-def pay(source, target, asset_code, issuer, amount):
+assert get_bul_balance(ISSUER) is None
+
+
+def pay(source, target, amount):
     builder = stellar_base.builder.Builder(secret=source.seed())
-    builder.append_payment_op(target, amount, asset_code, issuer)
+    builder.append_payment_op(target.address().decode(), amount, 'BUL', ISSUER.address().decode())
     builder.sign()
     return builder.submit()
 
 
-ISSUER = get_keypair(random_seed())
-DISTRIBUTOR = get_keypair(random_seed())
-ISH = get_keypair(random_seed())
-print([fund_and_trust(account, 'BUL', ISSUER.address().decode()) for account in [ISSUER, DISTRIBUTOR, ISH]])
-print(pay(ISSUER, DISTRIBUTOR.address().decode(), 'BUL', ISSUER.address().decode(), 10))
-print([get_details(account.address().decode()).balances for account in [ISSUER, DISTRIBUTOR, ISH]])
-print(pay(DISTRIBUTOR, ISH.address().decode(), 'BUL', ISSUER.address().decode(), 1))
-print([get_details(account.address().decode()).balances for account in [ISSUER, DISTRIBUTOR, ISH]])
+def fund_buls(account):
+    try:
+        balance = get_bul_balance(account)
+        assert balance >= 100
+    except AssertionError:
+        pay(ISSUER, account, 100 - balance)
+        balance = get_bul_balance(account)
+    return (account.seed()[:5], balance)
+
+
+def fund_participants():
+    for account in PARTICIPANTS:
+        print(fund_buls(account))
+
+
+fund_participants()
+
+
+def print_balances():
+    for account in PARTICIPANTS:
+        print(account.seed()[:5], get_bul_balance(account))
+
+
+print_balances()
+
+
+def launch(launcher, courier, recipient, payment, collateral):
+    escrow = get_keypair(random_seed())
+    builder = stellar_base.builder.Builder(secret=courier.seed())
+    builder.append_create_account_op(escrow.address().decode(), 1)
+    builder.submit()
+    get_bul_balance(escrow)
+    builder = stellar_base.builder.Builder(secret=courier.seed())
+    builder.append_set_options_op(
+        master_weight=2, low_threshold=2, med_threshold=2, high_threshold=4)
+    builder.append_set_options_op(signer_address=recipient.address().decode())
+    builder.append_set_options_op(signer_address=launcher.address().decode())
+    builder.sign()
+    builder.submit()
+    pay(launcher, escrow, payment)
+    pay(courier, escrow, collateral)
+    return get_bul_balance(escrow)
+
+
+print(launch(LAUNCHER, COURIER, RECIPIENT, 10, 50))
+print_balances()
